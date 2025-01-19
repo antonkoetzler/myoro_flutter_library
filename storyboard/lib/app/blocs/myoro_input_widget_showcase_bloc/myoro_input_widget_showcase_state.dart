be708ddaa25d27fb741245b75cfbca4afff1fa1d
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com]>
2025-01-19 02:52:32 -0300

    q

--- storyboard/lib/app/blocs/myoro_input_widget_showcase_bloc/myoro_input_widget_showcase_state.dart
+++ storyboard/lib/app/blocs/myoro_input_widget_showcase_bloc/myoro_input_widget_showcase_state.dart
@@ -12,19 +12,15 @@ final class MyoroInputWidgetShowcaseState extends Equatable {
 
   /// [MyoroInputConfiguration.inputTextStyle].
   final TextStyle? inputTextStyle;
-  final bool inputTextStyleEnabled;
 
   /// [MyoroInputConfiguration.label].
   final String? label;
-  final bool labelEnabled;
 
   /// [MyoroInputConfiguration.placeholder].
   final String? placeholder;
-  final bool placeholderEnabled;
 
   /// [MyoroInputConfiguration.labelTextStyle].
   final TextStyle? labelTextStyle;
-  final bool labelTextStyleEnabled;
 
   /// If [MyoroInputConfiguration.suffix] will not be null.
   final bool suffixEnabled;
@@ -46,13 +42,9 @@ final class MyoroInputWidgetShowcaseState extends Equatable {
     this.inputStyle = MyoroInputStyleEnum.outlined,
     this.textAlign = TextAlign.start,
     this.inputTextStyle,
-    this.inputTextStyleEnabled = false,
     this.label,
-    this.labelEnabled = false,
     this.placeholder,
-    this.placeholderEnabled = false,
     this.labelTextStyle,
-    this.labelTextStyleEnabled = false,
     this.suffixEnabled = false,
     this.enabled = true,
     this.readOnly = false,
@@ -64,37 +56,29 @@ final class MyoroInputWidgetShowcaseState extends Equatable {
     MyoroInputWidgetShowcaseEnum? typeEnum,
     MyoroInputStyleEnum? inputStyle,
     TextAlign? textAlign,
+    bool textAlignEnabled = true,
     TextStyle? inputTextStyle,
-    bool? inputTextStyleEnabled,
+    bool inputTextStyleEnabled = true,
     String? label,
-    bool? labelEnabled,
+    bool labelEnabled = true,
     String? placeholder,
-    bool? placeholderEnabled,
+    bool placeholderEnabled = true,
     TextStyle? labelTextStyle,
-    bool? labelTextStyleEnabled,
+    bool labelTextStyleEnabled = true,
     bool? suffixEnabled,
     bool? enabled,
     bool? readOnly,
     bool? showClearTextButton,
     bool? checkboxOnChangedEnabled,
   }) {
-    inputTextStyleEnabled = inputTextStyleEnabled ?? this.inputTextStyleEnabled;
-    labelEnabled = labelEnabled ?? this.labelEnabled;
-    placeholderEnabled = placeholderEnabled ?? this.placeholderEnabled;
-    labelTextStyleEnabled = labelTextStyleEnabled ?? this.labelTextStyleEnabled;
-
     return MyoroInputWidgetShowcaseState(
       typeEnum: typeEnum ?? this.typeEnum,
       inputStyle: inputStyle ?? this.inputStyle,
       textAlign: textAlign ?? this.textAlign,
       inputTextStyle: inputTextStyleEnabled ? (inputTextStyle ?? this.inputTextStyle) : null,
-      inputTextStyleEnabled: inputTextStyleEnabled,
       label: labelEnabled ? (label ?? this.label) : null,
-      labelEnabled: labelEnabled,
       placeholder: placeholderEnabled ? (placeholder ?? this.placeholder) : null,
-      placeholderEnabled: placeholderEnabled,
       labelTextStyle: labelTextStyleEnabled ? (labelTextStyle ?? this.labelTextStyle) : null,
-      labelTextStyleEnabled: labelTextStyleEnabled,
       suffixEnabled: suffixEnabled ?? this.suffixEnabled,
       enabled: enabled ?? this.enabled,
       readOnly: readOnly ?? this.readOnly,
@@ -110,11 +94,8 @@ final class MyoroInputWidgetShowcaseState extends Equatable {
       '  inputStyle: $inputStyle,\n'
       '  textAlign: $textAlign,\n'
       '  inputTextStyle: $inputTextStyle,\n'
-      '  inputTextStyleEnabled: $inputTextStyleEnabled,\n'
       '  label: $label,\n'
-      '  labelEnabled: $labelEnabled,\n'
       '  placeholder: $placeholder,\n'
-      '  placeholderEnabled: $placeholderEnabled,\n'
       '  labelTextStyle: $labelTextStyle,\n'
       '  suffixEnabled: $suffixEnabled,\n'
       '  enabled: $enabled,\n'
@@ -130,11 +111,8 @@ final class MyoroInputWidgetShowcaseState extends Equatable {
       inputStyle,
       textAlign,
       inputTextStyle,
-      inputTextStyleEnabled,
       label,
-      labelEnabled,
       placeholder,
-      placeholderEnabled,
       labelTextStyle,
       suffixEnabled,
       enabled,
---
+++ storyboard/test/app/blocs/myoro_input_widget_showcase_bloc_test.dart
@@ -0,0 +1,113 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:storyboard/app/blocs/myoro_input_widget_showcase_bloc/myoro_input_widget_showcase_bloc.dart';
+import 'package:myoro_flutter_library/myoro_flutter_library.dart';
+import 'package:storyboard/storyboard.dart';
+
+/// Unit test of [MyoroInputWidgetShowcaseBloc].
+void main() {
+  final MyoroInputWidgetShowcaseEnum formatterEnum = MyoroInputWidgetShowcaseEnum.fake();
+  final MyoroInputStyleEnum inputStyle = MyoroInputStyleEnum.fake();
+  final TextAlign? textAlign = faker.randomGenerator.boolean()
+      ? TextAlign.values[faker.randomGenerator.integer(
+          TextAlign.values.length,
+        )]
+      : null;
+  final String? label = faker.randomGenerator.boolean() ? faker.lorem.word() : null;
+  final String? placeholder = faker.randomGenerator.boolean() ? faker.lorem.word() : null;
+  final bool suffixEnabled = faker.randomGenerator.boolean();
+  final bool enabled = faker.randomGenerator.boolean();
+  final bool readOnly = faker.randomGenerator.boolean();
+  final bool showClearTextButton = faker.randomGenerator.boolean();
+  late final TextStyle? inputTextStyle, labelTextStyle;
+
+  setUpAll(() {
+    MyoroTypographyTheme.textTheme = createMyoroTextTheme(faker.randomGenerator.boolean());
+    inputTextStyle = faker.randomGenerator.boolean() ? MyoroTypographyTheme.instance.randomTextStyle : null;
+    labelTextStyle = faker.randomGenerator.boolean() ? MyoroTypographyTheme.instance.randomTextStyle : null;
+  });
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetFormatterEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetFormatterEvent(formatterEnum)),
+    expect: () => [MyoroInputWidgetShowcaseState(typeEnum: formatterEnum)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetInputStyleEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetInputStyleEvent(inputStyle)),
+    expect: () => [MyoroInputWidgetShowcaseState(inputStyle: inputStyle)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetTextAlignEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetTextAlignEvent(textAlign)),
+    expect: () => [MyoroInputWidgetShowcaseState(textAlign: textAlign ?? TextAlign.start)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetInputTextStyleEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetInputTextStyleEvent(inputTextStyle)),
+    expect: () => [MyoroInputWidgetShowcaseState(inputTextStyle: inputTextStyle)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetLabelEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetLabelEvent(label)),
+    expect: () => [MyoroInputWidgetShowcaseState(label: label)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetPlaceholderEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetPlaceholderEvent(placeholder)),
+    expect: () => [MyoroInputWidgetShowcaseState(placeholder: placeholder)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetLabelTextStyleEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetLabelTextStyleEvent(labelTextStyle)),
+    expect: () => [MyoroInputWidgetShowcaseState(labelTextStyle: labelTextStyle)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetSuffixEnabledEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetSuffixEnabledEvent(suffixEnabled)),
+    expect: () => [MyoroInputWidgetShowcaseState(suffixEnabled: suffixEnabled)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetEnabledEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetEnabledEvent(enabled)),
+    expect: () => [MyoroInputWidgetShowcaseState(enabled: enabled)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetReadOnlyEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetReadOnlyEvent(readOnly)),
+    expect: () => [MyoroInputWidgetShowcaseState(readOnly: readOnly)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetShowClearTextButtonEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+    act: (bloc) => bloc.add(SetShowClearTextButtonEvent(showClearTextButton)),
+    expect: () => [MyoroInputWidgetShowcaseState(showClearTextButton: showClearTextButton)],
+  );
+
+  blocTest(
+    'MyoroInputWidgetShowcaseBloc.SetCheckboxOnChangedEnabledEvent',
+    build: () => MyoroInputWidgetShowcaseBloc(),
+  );
+}
