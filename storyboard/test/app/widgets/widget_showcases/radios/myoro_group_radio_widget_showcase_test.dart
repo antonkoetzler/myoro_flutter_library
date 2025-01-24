9af857b5def6453e8b091b9b99a8fc5dd81fed6b
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-01-24 16:51:48 -0300

    why does all learning feel bad at first?

---
+++ storyboard/test/app/widgets/widget_showcases/radios/myoro_group_radio_widget_showcase_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myoro_flutter_library/myoro_flutter_library.dart';
+import 'package:storyboard/storyboard.dart';
+
+/// Widget test of [MyoroGroupRadioWidgetShowcase].
+void main() {
+  testWidgets('MyoroGroupRadioWidgetShowcase', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MyoroWidgetTester(
+        themeExtensionsBuilder: createStoryboardThemeExtensions,
+        child: BlocProvider(
+          create: (_) => WidgetShowcaseBloc(),
+          child: const MyoroGroupRadioWidgetShowcase(),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.byType(MyoroGroupRadioWidgetShowcase), findsOneWidget);
+    expect(find.byType(WidgetShowcase), findsOneWidget);
+    expect(find.byType(MyoroGroupRadio), findsOneWidget);
+    expect(
+      MyoroSingularDropdown.finder<Axis>(label: '[MyoroGroupRadio.direction]', labelEnabled: true),
+      findsOneWidget,
+    );
+    expect(
+      MyoroSlider.finder(label: '[MyoroGroupRadio.spacing]', labelEnabled: true),
+      findsOneWidget,
+    );
+    expect(
+      MyoroSlider.finder(label: '[MyoroGroupRadio.runSpacing]', labelEnabled: true),
+      findsOneWidget,
+    );
+  });
+}
