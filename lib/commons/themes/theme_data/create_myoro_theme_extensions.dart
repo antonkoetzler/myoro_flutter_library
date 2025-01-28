1d45b4c3fd7289fac4316a8e96f76948ffe33011
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-01-28 00:36:20 -0300

    Done for today

--- lib/commons/themes/theme_data/create_myoro_theme_extensions.dart
+++ lib/commons/themes/theme_data/create_myoro_theme_extensions.dart
@@ -35,7 +35,7 @@ List<ThemeExtension> createMyoroAppBarThemeExtensions(ColorScheme colorScheme) {
   return [
     MyoroAppBarThemeExtension(
       primaryColor: colorScheme.primary,
-      contentPadding: const EdgeInsets.fromLTRB(10, 5, 5, 5),
+      contentPadding: const EdgeInsets.all(5),
     ),
   ];
 }
