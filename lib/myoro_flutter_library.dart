65a061ab885814ba5f4a7a493aa29ddf1844c9e9
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-03-18 15:56:18 -0300

    Remove commons and core folders

--- lib/myoro_flutter_library.dart
+++ lib/myoro_flutter_library.dart
@@ -1,3 +1,14 @@
-export 'commons/exports.dart';
-export 'core/exports.dart';
+export 'abstracts/exports.dart';
+export 'blocs/exports.dart';
+export 'controllers/exports.dart';
+export 'databases/exports.dart';
+export 'enums/exports.dart';
+export 'extensions/exports.dart';
+export 'functions/exports.dart';
+export 'helpers/exports.dart';
+export 'input_formatters/exports.dart';
+export 'models/exports.dart';
+export 'notifiers/exports.dart';
+export 'themes/exports.dart';
+export 'widgets/exports.dart';
 export 'constants.dart';
