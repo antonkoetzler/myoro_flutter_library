18d7230cea8512a3441c5db998ce01ed1319dae4
Anton Louis Koetzler-Faust <66643637+antonkoetzler@users.noreply.github.com>
2025-06-29 14:56:56 -0300

    Update myoro_debounce.dart

--- lib/src/debouncing/myoro_debounce.dart
+++ lib/src/debouncing/myoro_debounce.dart
@@ -1,5 +1,7 @@
 import 'package:myoro_flutter_library/myoro_flutter_library.dart';
 
+// TODO: Needs to be refactored
+
 /// Function to debounce a function (apply a timer then execute [callback]).
 Future<void> myoroDebounce(Function callback, [Duration? duration]) async {
   await Future.delayed(duration ?? kMyoroDebounceDuration);
