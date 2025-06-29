d2aba1fffe60fe59473b730f0d7104de727ce534
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-06-29 18:35:11 -0300

    savweqe

--- lib/src/database/myoro_local_database.dart
+++ lib/src/database/myoro_local_database.dart
@@ -12,33 +12,37 @@ class MyoroLocalDatabase {
   /// Instance of [MyoroLocalDatabase] so we don't recreate a
   /// [MyoroLocalDatabase] object everytime [instance] is called.
   static final _instance = MyoroLocalDatabase();
+  static MyoroLocalDatabase get instance {
+    assert(
+      _database != null,
+      '[MyoroLocalDatabase]: Database not initialized; please call [MyoroLocalDatabase.initialize].',
+    );
+    return _instance;
+  }
 
   /// Instance of the SQLite database.
   static Database? _database;
 
   /// Initializes the database.
   static Future<void> initialize({
-    /// Name of the database, i.e. providing 'Myoro Fitness' would create 'Myoro Fitness.db'.
+    /// Name of the database, i.e. providing 'myoro_matchup' would create 'myoro_matchup.db'.
     required String fileName,
 
-    /// Alternative path to the database. Default is:
+    /// Alternative path to the database.
     String? path,
 
     /// Raw SQL to execute.
     String? sql,
 
     /// For debugging, used to always create a new .db instead of opening an
-    /// existing database. WILL DELETE YOUR DATABASE IF USED, YOU ARE WARNED.
+    /// existing database. WILL DELETE YOUR DATABASE IF USED; YOU ARE WARNED.
     bool cleanRun = false,
   }) async {
     // Initialize SQLite FFI.
     databaseFactory = databaseFactoryFfi;
 
     // Path to the database on the computer.
-    final formattedPath = join(
-      path ?? (await getApplicationSupportDirectory()).path,
-      '$fileName.db',
-    );
+    final formattedPath = join(path ?? (await getApplicationSupportDirectory()).path, '$fileName.db');
 
     // Opening database at [formattedPath] or creating a new database there.
     if (cleanRun) {
@@ -48,8 +52,9 @@ class MyoroLocalDatabase {
     _database = await openDatabase(formattedPath);
 
     // Table initialization
-    if (sql == null) return;
-    _database!.execute(sql);
+    if (sql != null) {
+      _database!.execute(sql);
+    }
   }
 
   /// Closes the database.
@@ -58,35 +63,18 @@ class MyoroLocalDatabase {
     _database = null;
   }
 
-  /// Getter to get [_instance] to use [MyoroLocalDatabase]'s functions.
-  static MyoroLocalDatabase get instance {
-    assert(
-      _database != null,
-      '[MyoroLocalDatabase]: Database not initialized; please call [MyoroLocalDatabase.initialize].',
-    );
-    return _instance;
-  }
-
   /// INSERT operation. Returns the ID of the newly created row or throws if unsuccessful.
   Future<int> insert(String tableName, {required Map<String, dynamic> data}) async {
     return await _database!.insert(tableName, data);
   }
 
   /// SELECT operation. Returns the list of a given query.
-  Future<List<Map<String, dynamic>>> select(
-    String tableName, {
-    String? where,
-    List<Object?>? whereArgs,
-  }) async {
+  Future<List<Map<String, dynamic>>> select(String tableName, {String? where, List<Object?>? whereArgs}) async {
     return await _database!.query(tableName, where: where, whereArgs: whereArgs);
   }
 
   /// Get operation. Selects the first row of a [select] operation or null if the result of the [select] call is empty.
-  Future<Map<String, dynamic>?> get(
-    String tableName, {
-    String? where,
-    List<Object?>? whereArgs,
-  }) async {
+  Future<Map<String, dynamic>?> get(String tableName, {String? where, List<Object?>? whereArgs}) async {
     final rows = await select(tableName, where: where, whereArgs: whereArgs);
     return rows.isEmpty ? null : rows.first;
   }
--- test/src/database/myoro_local_database_test.dart
+++ test/src/database/myoro_local_database_test.dart
@@ -1,3 +1,39 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:myoro_flutter_library/myoro_flutter_library.dart';
 
-void here
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const fileName = 'myoro';
+  const path = './';
+
+  group('MyoroLocalDatabase.instance', () {
+    test('MyoroLocalDatabase.instance not initialized assertion case', () {
+      expect(() => MyoroLocalDatabase.instance, throwsAssertionError);
+    });
+  });
+
+  group('MyoroLocalDatabase.initialize', () {
+    setUp(() {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+        const MethodChannel('plugins.flutter.io/path_provider'),
+        (methodCall) async {
+          return methodCall.method == 'getApplicationSupportDirectory' ? path : null;
+        },
+      );
+    });
+
+    test('Minimal MyoroLocalDatabase.initialize case', () async {
+      await MyoroLocalDatabase.initialize(fileName: fileName);
+      expect(() => MyoroLocalDatabase.instance, returnsNormally);
+      MyoroLocalDatabase.instance.close();
+    });
+
+    test('MyoroLocalDatabase.initialize with path argument provided.', () async {
+      await MyoroLocalDatabase.initialize(fileName: fileName, path: path);
+      expect(() => MyoroLocalDatabase.instance, returnsNormally);
+      MyoroLocalDatabase.instance.close();
+    });
+  });
+}
