511533fb8b46ca620802c2b2de78af8de7ab38a8
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-07-10 20:58:01 -0300

    Add todo

--- lib/src/widget/shared/abstract/myoro_stateful_widget.dart
+++ lib/src/widget/shared/abstract/myoro_stateful_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 /// [StatefulWidget] abstract for an MFL [Widget].
+///
+/// TODO: DELETE THIS, use get it and injectable, and make every view model use @injectable as it
+/// creates a new instance of the injectable object everytime it's called which makes this bad impl.
 abstract class MyoroStatefulWidget extends StatefulWidget {
   const MyoroStatefulWidget({super.key, this.createViewModel = true});
 
