a0f2913d672ac13bd69693c042535812b6ab4d50
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-07-11 15:38:41 -0300

    okay

---
+++ lib/src/widget/core/dependency_injection/init_get_it.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:myoro_flutter_library/src/widget/core/dependency_injection/init_get_it.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit()
+void configureGetIt() {
+  getIt.init();
+}
+qjweioqwjeoiqwjeoiqjeqwoiej
+final _ = configureGetIt();
